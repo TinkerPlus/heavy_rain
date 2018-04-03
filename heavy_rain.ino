eedc591d2ad60d564debb85761347b7f53b4f647
trevery <canjia.mail@gmail.com>
2018-04-03 11:42:22 +0800

    change interrupt to polling , sending to different addr is success

--- heavy_rain.ino
+++ heavy_rain.ino
@@ -22,6 +22,11 @@ IPAddress destIp(192,168,1,255);   // remote IP of the target device
 const unsigned int destPort = 8888;    // remote port of the target device where the NodeMCU sends OSC to
 const unsigned int localPort = 8888;   // local port to listen for UDP packets at the NodeMCU (another device must send OSC messages to this port)
 
+IPAddress IPlist[3][2] = {
+                              {IPAddress(192,168,1,216),IPAddress(192,168,1,218)},
+                              {IPAddress(192,168,1,217)},
+                              {IPAddress(192,168,1,238)}
+                            };
 
 void setup() {
   Serial.begin(115200);
@@ -49,30 +54,93 @@ void setup() {
   Serial.println(Udp.localPort());
 
   pinMode(ledPin, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(btnPin), btnIntCallBack, FALLING);
+  digitalWrite(ledPin, HIGH);
+  //attachInterrupt(digitalPinToInterrupt(btnPin), btnIntCallBack, FALLING);
 }
 
 void btnIntCallBack(){
+  delay(2000);
   Serial.println("Key Pressed");
   //irsend.sendNEC(0xFFB04F, 32);
+  detachInterrupt(digitalPinToInterrupt(btnPin));
   digitalWrite(ledPin, LOW);
-  sendOSC();
+  //sendOSC();
+  attachInterrupt(digitalPinToInterrupt(btnPin), btnIntCallBack, FALLING);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  receiveOSC();
-}
-
-void sendOSC(){
+void sendOSCTo(int addr, int state){
     OSCMessage msgOut("/color");
-    msgOut.add(0);
+    msgOut.add(state);
     // send via udp
-    Udp.beginPacket(destIp, destPort);
+    Udp.beginPacket(IPAddress(192,168,1,addr), 8888);
     msgOut.send(Udp);
     Udp.endPacket();
     msgOut.empty();
 }
+//void sendOSC(){
+//    OSCMessage msgOut("/color");
+//    msgOut.add(0);
+//    // send via udp
+//    Udp.beginPacket(IPAddress(192,168,1,217), 8888);
+//    msgOut.send(Udp);
+//    Udp.endPacket();
+//    delay(1000);
+//    Udp.beginPacket(IPAddress(192,168,1,216), 8888);
+//    msgOut.send(Udp);
+//    Udp.endPacket();
+//    msgOut.empty();
+//}
+//void sendOSC(){
+//    OSCMessage msgOut("/color");
+//    msgOut.add(0);
+//    // send via udp
+//    for(int i=0; i<3; i++){
+//      for(int j=0; j<2; j++){
+//        Udp.beginPacket(IPlist[i][j], destPort);
+//        msgOut.send(Udp);
+//        Udp.endPacket();
+//      }
+//      delay(1000);
+//    }
+//    msgOut.empty();
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(btnPin) == 0){
+    delay(100);
+    if(digitalRead(btnPin) == 1){
+       Serial.println("KeyPressed");
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      sendOSCTo(216,0);
+      delay(500);
+      sendOSCTo(216,1);
+      sendOSCTo(217,0);
+      delay(500);
+      sendOSCTo(216,0);
+      sendOSCTo(217,1);
+      delay(500);
+      sendOSCTo(216,1);
+      sendOSCTo(217,0);
+    }
+  }
+  receiveOSC();
+}
+
+//void sendOSC(){
+//    OSCMessage msgOut("/color");
+//    msgOut.add(0);
+//    // send via udp
+//    for(int i=0; i<3; i++){
+//      for(int j=0; j<2; j++){
+//        Udp.beginPacket(IPlist[i][j], destPort);
+//        msgOut.send(Udp);
+//        Udp.endPacket();
+//      }
+//      delay(1000);
+//    }
+//    msgOut.empty();
+//}
+
 
 void receiveOSC(){
   OSCMessage msgIN;
